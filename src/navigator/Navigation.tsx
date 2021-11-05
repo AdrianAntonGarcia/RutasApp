3b9cd17300acc5bc7ec00123dcc1aa8ebfdcf95f
adriton <adrtler@gmail.com>
2021-11-05 16:54:37 +0100

    Mostrar pantalla en función de los permisos

--- src/navigator/Navigation.tsx
+++ src/navigator/Navigation.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {MapScreen} from '../screens/MapScreen';
 import {PermissionsScreen} from '../screens/PermissionsScreen';
+import {permissionsContext} from '../conext/PermissionsContext';
+import {LoadingScreen} from '../screens/LoadingScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
+  const {permissions} = useContext(permissionsContext);
+  if (permissions.locationStatus === 'unavailable') {
+    return <LoadingScreen />;
+  }
   return (
     <Stack.Navigator
-      initialRouteName="PermissionsScreen"
+      // initialRouteName={
+      //   permissions.locationStatus === 'granted'
+      //     ? 'MapScreen'
+      //     : 'PermissionsScreen'
+      // }
       screenOptions={{
         headerShown: false,
         cardStyle: {backgroundColor: 'white'},
       }}>
-      <Stack.Screen name="MapScreen" component={MapScreen} />
-      <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+      {permissions.locationStatus === 'granted' ? (
+        <Stack.Screen name="MapScreen" component={MapScreen} />
+      ) : (
+        <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+      )}
     </Stack.Navigator>
   );
 };
--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
 
 export const LoadingScreen = () => {
   return (
-    <View>
-      <Text>LoadingScreen</Text>
+    <View style={styles.container}>
+      <ActivityIndicator color="black" size={50} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
