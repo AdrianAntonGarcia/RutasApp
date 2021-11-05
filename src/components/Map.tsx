d8c7bc89eb9aefb7683a94a7c49fab5591547530
adriton <adrtler@gmail.com>
2021-11-05 20:08:06 +0100

    Obtener las coordenadas del usuario

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MapView, {Marker} from 'react-native-maps';
-
+import Geolocation from '@react-native-community/geolocation';
 interface Props {
   markers?: Marker[];
 }
 export const Map = ({markers}: Props) => {
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      info => console.log(info),
+      err => console.log({err}),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+      },
+    );
+  }, []);
   return (
     <>
       <MapView
         style={{flex: 1}}
+        showsUserLocation
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 39.4964,
+          longitude: -0.373057,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}>
